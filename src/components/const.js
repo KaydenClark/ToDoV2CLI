f92333c0b76796410863767e5ccaf06985504527
Kayden Clark <kaydenclark725@gmail.com>
2021-01-26 18:44:15 -0700

    first commit

---
+++ src/components/const.js
@@ -0,0 +1,11 @@
+const baseProd = 'https://kc-todolist-api.herokuapp.com' //Production
+
+const base = 'http://localhost:5000' //Testing
+
+const currentAPI = ""
+
+
+
+export {
+    base, currentAPI, baseProd
+}
